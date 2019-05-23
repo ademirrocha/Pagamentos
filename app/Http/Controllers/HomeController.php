7fa12e04a2b03182053e5dd4c226bbdbc0ee0be6
ademirrocha <tiademir.rocha93@gmail.com>
2019-05-23 03:42:31 -0300

    correcao

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
                                 ->orWhere('returnCode', 1)
                                 ->count();
 
-        $qtdPaymentsLocalTotal = PaymentsApi::count();
+        $qtdPaymentsLocalTotal = PaymentsLocal::count();
 
         $qtdPaymentsApi = PaymentsApi::where('returnCode', 4)
                                 ->orWhere('returnCode', 1)
