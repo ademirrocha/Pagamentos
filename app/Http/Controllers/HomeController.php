822513f9bc0f8d98129a1446cbca79a7ac4975b3
ademirrocha <tiademir.rocha93@gmail.com>
2019-05-23 03:11:04 -0300

    correcal de a para A

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Models\Local\Payments\Payments as PaymentsLocal;
-use App\Models\api\Payments\Payments as PaymentsApi;
+use App\Models\Api\Payments\Payments as PaymentsApi;
 
 class HomeController extends Controller
 {
