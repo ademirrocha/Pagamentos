144acc1b56064b1e9b9be7d101ff40f3763b3d7a
ademirrocha <tiademir.rocha93@gmail.com>
2019-06-02 21:36:49 -0300

    update maps

--- app/Http/Controllers/Api/Googlmapper/MapsController.php
+++ app/Http/Controllers/Api/Googlmapper/MapsController.php
@@ -275,6 +275,18 @@ function initMap() {
             directionsDisplay);
 }
 
+
+function computeTotalDistance(result) {
+        var total = 0;
+        var myroute = result.routes[0];
+        for (var i = 0; i < myroute.legs.length; i++) {
+          total += myroute.legs[i].distance.value;
+        }
+        total = total / 1000;
+        document.getElementById('total').innerHTML = total + ' km';
+      }
+
+
 /**
  * @constructor
  */
